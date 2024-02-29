d312dbc2037084f64f56e2d8f8d1640a924849c3
sjals93 <sjals93@gmail.com>
2024-02-29 11:23:21 +0900

    Update autoaugment.py

--- autoaugment/autoaugment.py
+++ autoaugment/autoaugment.py
@@ -65,8 +65,11 @@ def blend(image1, image2, factor):
   if factor == 1.0:
     return tf.convert_to_tensor(image2)
 
-  image1 = tf.to_float(image1)
-  image2 = tf.to_float(image2)
+  # image1 = tf.to_float(image1)
+  image1 = tf.cast(image1, tf.float32)
+
+  # image2 = tf.to_float(image2)
+  image2 = tf.cast(image2, tf.float32)
 
   difference = image2 - image1
   scaled = factor * difference
